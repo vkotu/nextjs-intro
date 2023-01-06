9693808f583a209d97b2a8365226426faeff7fe3
vkotu <vkotu@paypal.com>
2023-01-06 11:16:23 -0800

    dynamic import

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,14 @@
 /** @jsxImportSource theme-ui */
 import { jsx } from 'theme-ui'
+// import Browser from '../src/components/browser';
+
+import dynamic from 'next/dynamic';
+
+const BrowserComponent = dynamic(() => import('../src/components/browser'), {ssr: false})
+
 const index =  ({content}) => (
   <div sx={{ height: `calc(100vh - 60px)`}}>
+    <BrowserComponent />
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
       <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
     </div>
---
+++ src/components/browser.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+console.log(document.body);
+export default function Browser() {
+    return <div>Browser component</div>
+}
